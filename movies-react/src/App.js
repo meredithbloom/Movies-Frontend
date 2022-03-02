b14e8c0ead28b7a9140827c135b664657fd48505
Heather Mielke <heatherlmielke@gmail.com>
2022-03-02 12:58:49 -0600

    added connection to movie api

--- movies-react/src/App.js
+++ movies-react/src/App.js
@@ -2,13 +2,15 @@ import {useState, useEffect} from 'react'
 import axios from 'axios'
 
 const App = () => {
-  const [topmovies, setTopMovies] = useState([])
+  const [topRated, setTopRated] = useState([])
   const [recommended, setRecommended] = useState([])
   const [favorites, setFavorites] = useState([])
   const [wishList, setWishList] = useState([])
 
 const getMovies = () => {
-//axios.get
+  axios.get('https://api.themoviedb.org/3/movie/top_rated?api_key=dad5072890b975c25ff2358e550e5138&language=en-US&page=1').then((response) => {
+    setTopRated(response.data.results)
+  })
 }
 
 useEffect(() => {
@@ -18,6 +20,17 @@ useEffect(() => {
   return(
     <>
       <h1>Movies</h1>
+      <div>
+      {
+      topRated.map((movie, index) => {
+        return(
+          <div key={movie.id}>
+          <p>Title: {movie.title} </p>
+          
+          </div>
+        )})
+    }
+      </div>
     </>
   )
 }
