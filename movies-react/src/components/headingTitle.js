d61c843e9ededd3d6ffda3c01c438d64017b093d
Heather Mielke <heatherlmielke@gmail.com>
2022-03-03 09:43:49 -0600

    added title component and some bootstrap styling

---
+++ movies-react/src/components/headingTitle.js
@@ -0,0 +1,10 @@
+
+const MovieType = (props) => {
+  return(
+    <div className = 'col'>
+      <h2 className='movie-heading'>{props.heading}</h2>
+    </div>
+  )
+}
+
+export default MovieType
--- movies-react/src/components/trendingmovies.js
+++ movies-react/src/components/trendingmovies.js
@@ -5,12 +5,14 @@ const TrendingMovies = (props) => {
     return(
         props.trendingMovies.map((movie) => {
             let img = movie.poster_path
-            let full = 'http://image.tmdb.org/t/p/w185' + img;
+            let full = 'http://image.tmdb.org/t/p/w200' + img;
             return(
-                <div key={movie.id} className="trending-movie">
-                    <h3>{movie.title}</h3>
-                    <p>{movie.overview}</p>
+                <div key={movie.id} className="image-container d-flex justify-content-start m-2 col">
                     <img src= {full}/>
+                    <div className ='overlay d-flex align-items-center justify-content-center'>
+                      {movie.title}
+
+                    </div>
                 </div>
             )
         })
