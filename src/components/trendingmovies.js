7c72edc0eb856d7316f02aaeb20ee03429823184
Heather Mielke <heatherlmielke@gmail.com>
2022-03-04 09:01:04 -0600

    code

---
+++ src/components/trendingmovies.js
@@ -0,0 +1,35 @@
+import axios from 'axios'
+
+
+const TrendingMovies = (props) => {
+    return(
+        props.trendingMovies.map((movie) => {
+            let img = movie.poster_path
+            let full = 'http://image.tmdb.org/t/p/w200' + img;
+            return(
+                <div key={movie.id} className="image-container d-flex justify-content-start m-3 col">
+                    <img src= {full}/>
+                    <div className ='overlay d-flex flex-row align-items-start justify-content-between'>
+                    <div>
+                      <p className='movie-title text-left'>{movie.title}</p>
+                      <p className="year">Year:{movie.release_date.substring(0,4)}</p>
+                      </div>
+                      <div className="d-flex flex-column justify-content-around">
+                        <svg className="m-1 icon heart-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
+                        </svg>
+                        <svg className="m-1 icon plus-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
+                        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/>
+                        </svg>
+                        <svg className="m-1 icon check-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 16 16">
+                        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+                        </svg>
+                      </div>
+                    </div>
+                </div>
+            )
+        })
+    )
+}
+
+export default TrendingMovies
